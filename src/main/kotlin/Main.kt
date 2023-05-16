f834b132c0d1306398bb62dc6187599e005cb715
nicol <nicolasfeksa658@gmail.com>
2023-05-16 20:01:13 -0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,21 @@
+fun main() {
+    var meuCarro = Carros()
+    meuCarro.modelo = "Chevette"
+    meuCarro.marca = "Chevrolet"
+    meuCarro.ano = 1987
+    meuCarro.preco = 14990.00
+    println("Modelo:${meuCarro.modelo}")
+    println("Marca: ${meuCarro.marca}")
+    println("Ano: ${meuCarro.ano}")
+    println("Preço: ${meuCarro.preco}")
+}
+/*
+    Uma variável é um nome que se refere a um valor armazenado na memória do computador.
+    Esse valor pode ser de diversos tipos, como inteiro, real, booleano, caractere, entre outros.
+    Uma variável é identificada por um nome único, que é utilizado para acessar o valor armazenado nela.
+    Já um objeto é uma instância de uma classe. Uma classe é uma estrutura que define um conjunto de atributos e métodos que representam um tipo de dado abstrato.
+    Um objeto é uma entidade concreta criada a partir de uma classe, que possui valores para seus atributos e pode executar seus métodos.
+    Resumindo, uma variável é um nome que se refere a um valor armazenado na memória, enquanto um objeto é uma instância de uma classe,
+    que possui valores para seus atributos e pode executar seus métodos.
+
+ */
---
+++ src/main/kotlin/class.kt
@@ -0,0 +1,8 @@
+class Carros {
+    var modelo:String = ""
+    var marca: String = ""
+    var ano: Int = 0
+    var preco: Double = 0.0
+
+}
+
